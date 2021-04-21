9390b19f85830a84597bdd25377e23c829d5d676
k1mjiho <jjiho0126@gmail.com>
2021-04-21 19:26:41 +0900

    스킬트리 시간복잡도개선

--- Programmers/스킬트리.py
+++ Programmers/스킬트리.py
@@ -1,4 +1,26 @@
-def solution(skill, skill_trees):
+'''
+2021-04-21 시간복잡도 해결해서 추가
+filter, startswith 라이브러리 사용하기
+list 안에 있는 지 확인할 때는 list보다 set을 사용하자 시간복잡도 O(N) -> O(1)로 줄일 수 있다
+매개변수를 자신이 편한걸로 바꾸자
+'''
+def is_valid(skill, lst):
+    if skill.startswith(lst):
+        return True
+    else:
+        return False
+
+def solution2(skills_order, candidates):
+    answer = 0
+
+    for candidate in candidates:
+        filtered = filter(lambda skill: skill in skills_order, candidate)
+        if is_valid(skills_order, ''.join(filtered)):
+            answer += 1
+    return answer
+
+
+def solution1(skill, skill_trees):
     answer = 0
 
     for trees in skill_trees:
@@ -16,3 +38,4 @@ def solution(skill, skill_trees):
             answer += 1
 
     return answer
+
